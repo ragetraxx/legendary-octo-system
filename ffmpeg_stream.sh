ebd05baa40d9556ddc00a0e78c204da9513c095a
ragetraxx <93855827+ragetraxx@users.noreply.github.com>
2025-03-17 17:42:34 +0800

    Update ffmpeg_stream.sh

--- ffmpeg_stream.sh
+++ ffmpeg_stream.sh
@@ -1,4 +1,7 @@
-ffmpeg -f rawvideo -pixel_format bgr24 -video_size 1920x1080 -framerate 30 -i - \
-       -i "https://stream.zeno.fm/q1n2wyfs7x8uv" -c:v libx264 -preset ultrafast -b:v 3000k -g 60 \
-       -c:a aac -b:a 128k -strict experimental -f flv \
-       "rtmp://ssh101.bozztv.com:1935/ssh101/ragemusicph"
+#!/bin/bash
+
+ffmpeg -re \
+       -i "https://stream.zeno.fm/q1n2wyfs7x8uv" \
+       -c:v libx264 -preset ultrafast -tune zerolatency -b:v 2000k -g 30 \
+       -c:a aac -b:a 128k -strict experimental -bufsize 4000k \
+       -f flv "rtmp://ssh101.bozztv.com:1935/ssh101/ragemusicph"
