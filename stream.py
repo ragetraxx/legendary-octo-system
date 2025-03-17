92c00db33990bdb66b3531db5dfc98257bcfeb9e
ragetraxx <93855827+ragetraxx@users.noreply.github.com>
2025-03-17 23:44:48 +0800

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 
 # Stream and output settings
@@ -6,7 +5,7 @@ AUDIO_URL = "https://stream.zeno.fm/q1n2wyfs7x8uv"
 RTMP_URL = "rtmp://ssh101.bozztv.com:1935/ssh101/ragemusicph"
 BACKGROUND_IMAGE = "background.png"
 
-# FFmpeg command
+# FFmpeg command (using avectorscope instead of showcqt)
 ffmpeg_cmd = [
     "ffmpeg", "-re",
     "-probesize", "32", "-analyzeduration", "0",
@@ -15,7 +14,7 @@ ffmpeg_cmd = [
     "-filter_complex",
     "[1:v]scale=1280:720[bg];"
     "[bg]drawtext=text='%{localtime\\:%H\\:%M\\:%S}':fontcolor=white:fontsize=50:x=1000:y=30[clock];"
-    "[0:a]avectorscope=s=1280x200:rate=25:rc=0:gc=0:bc=255[visual];"
+    "[0:a]avectorscope=s=1280x200:rate=25:rc=255:gc=0:bc=0[visual];"
     "[clock][visual]overlay=x=0:y=520[out]",
     "-map", "[out]", "-map", "0:a",
     "-c:v", "libx264", "-preset", "ultrafast", "-tune", "stillimage",
